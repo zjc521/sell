abdcad7edb0806e650923167d1641266482d2de8
zjc <907545162@qq.com>
2019-06-08 20:29:48 +0800

    123

--- src/main/java/com/imooc/sell/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/sell/service/impl/OrderServiceImpl.java
@@ -1,20 +1,71 @@
 package com.imooc.sell.service.impl;
 
+import com.imooc.sell.dataobject.OrderDetail;
+import com.imooc.sell.dataobject.OrderMaster;
+import com.imooc.sell.dataobject.ProductInfo;
+import com.imooc.sell.dto.CartDTO;
 import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.OrderStatusEnum;
+import com.imooc.sell.enums.PayStatusEnum;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
+import com.imooc.sell.repository.OrderDetailRepository;
+import com.imooc.sell.repository.OrderMasterRepository;
 import com.imooc.sell.service.OrderService;
 import com.imooc.sell.service.ProductService;
+import com.imooc.sell.utils.KeyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.awt.print.Pageable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
-        return null;
+        String orderId = KeyUtil.genUniqueKey();
+        BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+        for(OrderDetail orderDetail : orderDTO.getOrderDetailList()){
+            ProductInfo productInfo = productService.finOne(orderDetail.getProductId());
+            if(productInfo == null){
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            //计算订单总价
+            orderAmount = productInfo.getProductPrice().multiply(new BigDecimal(orderDetail.getProductQuantity())).add(orderAmount);
+            //订单详情
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo,orderDetail);
+            orderDetailRepository.save(orderDetail);
+
+        }
+        //写入订单数据
+        OrderMaster orderMaster = new OrderMaster();
+        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderId(orderId);
+        orderMaster.setOrderAmount(orderAmount);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterRepository.save(orderMaster);
+
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e ->
+                new CartDTO(e.getProductId(),e.getProductQuantity())).collect(Collectors.toList());
+        productService.decreaseStock(cartDTOList);
+        return orderDTO;
     }
 
     @Override
