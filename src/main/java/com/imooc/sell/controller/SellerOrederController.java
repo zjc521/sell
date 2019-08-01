61eccc133794d3f2eea6d53661d6856f8724420d
123 <907545163@qq.com>
2019-08-01 17:57:34 +0800

    zjc的提交

--- src/main/java/com/imooc/sell/controller/SellerOrederController.java
+++ src/main/java/com/imooc/sell/controller/SellerOrederController.java
@@ -1,7 +1,10 @@
 package com.imooc.sell.controller;
 
 import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
 import com.imooc.sell.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,17 +18,37 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/seller/order")
+@Slf4j
 public class SellerOrederController {
     @Autowired
     private OrderService orderService;
     @GetMapping("/list")
-    public ModelAndView list(@RequestParam("page") Integer page,
-                             @RequestParam("size") Integer size,
+    public ModelAndView list(@RequestParam(value = "page",defaultValue = "1") Integer page,
+                             @RequestParam(value = "size",defaultValue = "10") Integer size,
                              Map<String,Object> map){
         PageRequest request = new PageRequest(page-1,size);
         Page<OrderDTO> orderDTOPage = orderService.findList(request);
         map.put("orderDTOPage",orderDTOPage);
+        map.put("currentPage",page);
+        map.put("size",size);
 
         return new ModelAndView("order/list",map);
+    }
+    @GetMapping("/cancel")
+    public ModelAndView cancel(@RequestParam("orderId") String orderId,
+                                Map<String,Object> map){
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+
+        }catch(SellException e){
+            log.error("【卖家端取消订单】 发生异常",e);
+            map.put("msg", ResultEnum.ORDER_NOT_EXIST);
+            map.put("url","/sell/seller/order/lsit");
+            return new ModelAndView("common/error",map);
+        }
+        map.put("msg",ResultEnum.SUCCESS.getMessage());
+        map.put("url","/sell/seller/order/list");
+        return new ModelAndView("common/success",map);
     }
 }
--- src/main/java/com/imooc/sell/service/PayService.java
+++ src/main/java/com/imooc/sell/service/PayService.java
@@ -1,18 +1,18 @@
-package com.imooc.sell.service;
+/*package com.imooc.sell.service;
 
 
 import com.imooc.sell.dto.OrderDTO;
 
-/**
+*//**
  * 支付
  * Created by 廖师兄
  * 2017-07-04 00:53
- */
+ *//*
 public interface PayService {
 
-   void create(OrderDTO orderDTO);
+   void create(OrderDTO orderDTO);*/
 
     /*PayResponse notify(String notifyData);
 
     RefundResponse refund(OrderDTO orderDTO);*/
-}
+//}
